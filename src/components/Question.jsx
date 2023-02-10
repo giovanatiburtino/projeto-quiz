9ede9d3dcb20ca6fcbbd25f320e97b0bd345bf6e
giova <giovanaftiburtino@gmail.com>
2023-02-10 16:07:21 -0300

    projeto finalizado

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { QuizContext } from '../context/quiz'
+import Option from './Option/Option'
 import './Question.css'
 
 const Question = () => {
@@ -7,14 +8,25 @@ const Question = () => {
 
     const currentQuestion = quizState.questions[quizState.currentQuestion]
 
+    const onSelectOption = (option) => {
+        dispatch({
+            type: "CHECK_ANSWER",
+            payload: {answer: currentQuestion.answer, option}
+        })
+    }
+
     return (
         <div id="question">
             <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
             <h2>{currentQuestion.question}</h2>
             <div id="options-container">
-                <p>Opções</p>
+                {currentQuestion.options.map((option) => (
+                    <Option option={option} key={option} answer={currentQuestion.answer} selectOption={() => onSelectOption(option)}/>
+                ))}
             </div>
+            {quizState.answerSelected && (
             <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>Continuar</button>
+            )}
         </div>
     )
 }
