21ccfafb5bea959551d131376b8f040c348b219a
giova <giovanaftiburtino@gmail.com>
2023-02-12 21:11:26 -0300

    ajuste svg

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { QuizContext } from '../../context/quiz';
-import Quiz from '../img/quiz.svg'
+import Quiz from '../../img/quiz.svg'
 
 import "./Welcome.css"
 
